570c85d81d6e79a35473ad2c6c4edd98c54b414a
Maleni <maleni.alexis@gmail.com>
2022-03-03 08:29:03 -0600

    test

---
+++ test1.js
@@ -0,0 +1,5 @@
+let x = myFunction(5, 7);   // Function is called, return value will end up in x
+
+function myFunction(a, b) {
+    return a + b;             // Function returns the product of a and b
+}
